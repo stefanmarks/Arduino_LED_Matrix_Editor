c375a01bbd9670fcd7b56f89c5b70f974d1e61b6
Stefan Marks <stefanmarks@users.noreply.github.com>
2024-06-17 08:36:34 +1200

    Moving serial code to main sketch

--- sketch.js
+++ sketch.js
@@ -40,10 +40,15 @@ var previewID    = 0;
 var previewsList = new Array();
 var baseFrame;
 
-let serialSendBuffer = new Uint32Array(3);
-
 var hoverCell;
 var lastHoverCell;
+
+var serialSendBuffer = new Uint32Array(3);
+var serialPort       = null;
+var serialPortReady  = false;
+var serialWriter;
+var serialPortCheckInterval;
+
 
 
 /* UI */
@@ -905,3 +910,58 @@ function deviceConnectedBack()
   showLiveTestDialog(true)
 }
 
+
+async function openPort() {
+  serialPort = await navigator.serial.requestPort();
+
+  /* on WebSerial connect/disconnect events seem not to be working */
+  serialPort.onconnect = function (event) {
+    console.log("onconnect: ", event);
+  }
+
+  await serialPort.open({ baudRate: 115200 }); // open the serial port
+
+  /* workaround to detect wether the port has been open */
+  if (serialPort.writable && serialPort.readable) {
+    setPortState(true);
+  }
+}
+
+
+function setPortState(_portState) {
+  console.log(_portState);
+  serialPortCheckInterval = setInterval(() => {
+
+    if (serialPort.writable && serialPort.readable) {
+      serialPortReady = true;
+      if (!serialLinkButton.selected) {
+        serialLinkButton.loadIcon("UIassets/link_on.png");
+        serialLinkButton.selected = true;
+        console.log("Serial Port Open");
+      }
+
+    } else {
+      serialPortReady = false;
+      if (serialLinkButton.selected) {
+        serialLinkButton.loadIcon("UIassets/link_off.png");
+        serialLinkButton.selected = false;
+        console.log("Serial Port Closed");
+      }
+    }
+  }, 100);
+}
+
+
+async function writeSerialData(_serialData) {
+  if (!serialWriter) {
+    serialWriter = serialPort.writable.getWriter(); // create a writable stream
+
+  }
+  if (serialPort.writable && serialPort.readable) {
+    // debugger;
+    // const encodedData = new Uint8Array([0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0xff, 0xff, 0xff]);
+    const encodedData = serialSendBuffer;
+    // debugger;
+    await serialWriter.write(encodedData);
+  }
+}
